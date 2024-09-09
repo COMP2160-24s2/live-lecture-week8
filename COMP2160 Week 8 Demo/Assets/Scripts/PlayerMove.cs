f2157480fe92b25515d208cc4438946e7ae8a06a
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2024-09-10 09:48:13 +1000

    Implemented player movement

---
+++ COMP2160 Week 8 Demo/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,62 @@
+/**
+ *
+ *
+ * Author: Malcolm Ryan
+ * Version: 1.0
+ * For Unity Version: 2022.3
+ */
+
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMove : MonoBehaviour
+{
+
+#region Parameters
+    [SerializeField] private float speed = 5;
+    [SerializeField] private int coinValue = 10;
+#endregion 
+
+#region Components
+#endregion
+
+#region State
+#endregion
+
+#region Actions
+    private Actions actions;
+    private InputAction moveAction;
+#endregion
+
+#region Init & Destroy
+    void Awake()
+    {
+        actions = new Actions();
+        moveAction = actions.movement.move;
+    }
+
+    void OnEnable() 
+    {
+        actions.movement.Enable();
+    }
+
+    void OnDisable() 
+    {
+        actions.movement.Disable();        
+    }
+#endregion Init
+
+#region Update
+    void Update()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+
+        transform.Translate(move * speed * Time.deltaTime);
+    }
+#endregion Update
+
+#region FixedUpdate
+
+#endregion FixedUpdate
+
+}
